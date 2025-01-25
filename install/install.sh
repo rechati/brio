3bc98b48f1f0ee98938a604ab2460ef24c473dfd
Lucas Eduardo <lukaszonecell@gmail.com>
2025-01-25 12:44:09 -0300

    Correct installer

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m)"
 
@@ -10,13 +12,23 @@ case $ARCH in
 esac
 
 REPO="rechati/brio"
-VERSION="latest"
-DOWNLOAD_URL="https://github.com/$REPO/releases/download/$VERSION/brio-$OS-$ARCH"
+VERSION="$(basename "$(curl -Ls -o /dev/null -w "%{url_effective}" "https://github.com/rechati/brio/releases/latest")")"
+TARBALL="brio-$VERSION-$OS-$ARCH.tar.gz"
+DOWNLOAD_URL="https://github.com/$REPO/releases/download/$VERSION/$TARBALL"
 
 INSTALL_DIR="/usr/local/bin"
 
+echo "$DOWNLOAD_URL"
 echo "Downloading brio..."
-sudo curl -L "$DOWNLOAD_URL" -o "$INSTALL_DIR/brio"
+curl -L "$DOWNLOAD_URL" -o "/tmp/$TARBALL"
+
+echo "Unpacking..."
+tar -xzf "/tmp/$TARBALL" -C /tmp/
+
+echo "Moving the binary to $INSTALL_DIR..."
+sudo mv /tmp/brio "$INSTALL_DIR/"
 sudo chmod +x "$INSTALL_DIR/brio"
+
+rm "/tmp/$TARBALL"
 
 echo "brio has been installed to $INSTALL_DIR/brio"
